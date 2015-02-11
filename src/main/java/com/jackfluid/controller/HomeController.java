21c1c6469451312f4038b39f3ec0e04e3445baf0
Christopher Yang <cyang@livegamer.com>
2015-02-10 17:30:37 -0800

    Adding loaderIO verificaiton.

--- src/main/java/com/jackfluid/controller/HomeController.java
+++ src/main/java/com/jackfluid/controller/HomeController.java
@@ -10,5 +10,10 @@ public class HomeController {
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+	
+	@RequestMapping("/loaderio-f4b4f879a17005b5bcd9a0f929da6e94/")
+	public String loadIOVerification(){
+		return "loaderio-f4b4f879a17005b5bcd9a0f929da6e94";
+	}
 
 }
